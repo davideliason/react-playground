d87d72fbee753c5b3c26009df846e4dbe416b5d1
David Eliason <david@davethemaker.com>
2018-06-14 16:23:55 -0700

    add babel presets to package.json

---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+var webpack = require('webpack');
+var path = require('path');
+
+var DEV = path.resolve(__dirname, "dev");
+var PRODUCTION = path.resolve(__dirname,"production");
+
+var config = {
+	entry: DEV + "/index.jsx",
+	output: {
+		path: PRODUCTION,
+		filename: "myCode.js"
+	}
+}
+
+module.exports = config;
