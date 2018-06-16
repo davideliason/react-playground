56f82e67b105dca7ae5e84e36ce76bc4e2a728e5
David Eliason <david@davethemaker.com>
2018-06-15 17:24:05 -0700

    add code from the Colorizer component that I had built yesterday to mini-app

--- src/Colorizer.js
+++ src/Colorizer.js
@@ -1,15 +1,68 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
 class Colorizer extends React.Component {
-	render(){
-		return (
-				 <div>
-   					 <h2>Colorizer</h2>
-  				 </div>
-				);
-			}
+	constructor(props){
+		super(props);
+		this.state = {
+			color: '',
+			bgColor: ''
+		}
+
+		this.colorValue = this.colorValue.bind(this);
+		this.setBGColor = this.setBGColor.bind(this);
+	}
+
+	colorValue(e) {
+		this.setState({
+			color: e.target.value
+		});
+	}
+
+	setBGColor(e) {
+		this.setState({
+			bgColor:this.state.color
+		});
+		this._input.value = "";
+		this._input.focus();
+
+		e.preventDefault();
 	}
 
 
+	render() {
+		var colorizerStyle = {
+			backgroundColor: this.state.bgColor,
+			width: 500,
+			height: 480,
+			padding: 0
+
+		}
+
+		var self = this;
+		
+// we set the ref attribute on the element we are interested in 
+// (the input field in this case.
+// We set a JS function which is called when that component is mounted.
+// Now, we can access the input element anywhere in the component
+// by calling ._input
+
+		return (
+				<div style={colorizerStyle}>
+				  <form onSubmit={this.setBGColor}>
+				    <input 
+				         ref={
+				         	function(el) {
+				         		self._input = el;
+				         	}
+				         } 
+				    	 onChange={this.colorValue}
+				    	 placeholder="enter new color">
+				    </input>
+				    <button type="submit">Enter</button>
+				  </form>
+				</div>
+			);
+	}
+}
 
 export default Colorizer;
