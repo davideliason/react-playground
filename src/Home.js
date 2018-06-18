b5f80eb7656182188aedbb66f7c4c04068d15abf
David Eliason <david@davethemaker.com>
2018-06-17 17:03:36 -0700

    update home page for the react-playground to give a teaser about the StarWars mini-app

--- src/Home.js
+++ src/Home.js
@@ -5,7 +5,8 @@ class Home extends React.Component {
 		return (
 				 <div>
    					 <h2>Home</h2>
-   					 <p>Here lie a variety of mini-apps exploring different JS, React, libraries. Enjoy!</p>
+   					 <p>Here lie a variety of mini-apps exploring different JS, React, libraries</p>
+   					 <p> 'Star Wars' displays proof of concept in that the child component receives state props from parent, and fetches API for content setting local state and rendering.</p>
   				 </div>
 				);
 			}
