ad1fd22d013e5873a7e5f558cefe878bd5d34441
David Eliason <david@davethemaker.com>
2018-06-19 11:48:32 -0700

    if component returns null then will not render

--- src/Sandbox.js
+++ src/Sandbox.js
@@ -1,10 +1,23 @@
 import React, {Component} from 'react';
 
+function WarningComponent(props) {
+	if(!props.warning) {
+		return null;
+	}
+
+	return (
+			<div>
+				<h3>Warning!</h3>
+			</div>
+		);
+}
+
 class Sandbox extends React.Component {
 	render() {
 		return (
 				<div>
 					<h3> Sandbox </h3>
+					<WarningComponent warning={true} /> 
 				</div>
 			);
 	}
